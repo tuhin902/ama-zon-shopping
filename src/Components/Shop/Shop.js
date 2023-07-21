7e54a99ac7249446c337b2f4cd47bda064ba90e1
tuhin902 <tuhin902md@gmail.com>
2023-07-21 11:23:45 +0600

    local storage workfinished

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -28,11 +28,23 @@ const Shop = () => {
         setCart(saveCart);
     }, [products])
 
-    const handleAddToCart = (product) => {
+    const handleAddToCart = (selectedProduct) => {
         // console.log(product);
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+
+        }
+
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     };
 
     return (
